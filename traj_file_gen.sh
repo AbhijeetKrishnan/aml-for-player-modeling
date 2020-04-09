45abf85f47db26036117f3c4202c3960ae805c8d
Abhijeet Krishnan <abhijeet.krishnan@gmail.com>
2020-04-09 15:22:34 -0700

    Add current files

---
+++ traj_file_gen.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+ENGINE=./trajectory/trajectory.py
+DOMAIN=./domain.pddl
+INSTANCES=./instances/*.pddl
+SOLUTIONS=./solutions/*
+LOGS=./logs
+
+inst_regex="instance-([0-9]+)\.pddl$"
+soln_regex="solution-([0-9]+)$"
+
+for instance in $INSTANCES
+do
+    if [[ $instance =~ $inst_regex ]]
+    then
+        id_inst=${BASH_REMATCH[1]}
+        for solution in $SOLUTIONS
+        do
+            if [[ $solution =~ $soln_regex ]]
+            then
+                id_soln=${BASH_REMATCH[1]}
+                if [ "$id_inst" == "$id_soln" ]
+                then
+                    echo $instance $solution
+                    log="${LOGS}/log-$id_soln"
+                    echo python "$ENGINE" "$DOMAIN" "$instance" "$solution" "$log"
+                    python "$ENGINE" "$DOMAIN" "$instance" "$solution" "$log"
+                fi
+            fi
+        done
+    fi
+done
